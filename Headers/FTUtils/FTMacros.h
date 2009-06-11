19899305dc104937dc2a1f1648af7ac1d0c2c585
Nathan Eror <bignate@gmail.com>
2009-06-11 17:38:09 -0500

    * Added basic documentation to the README  * Added the MIT License to all source files

--- Headers/FTUtils/FTMacros.h
+++ Headers/FTUtils/FTMacros.h
@@ -1,4 +1,28 @@
-// FTLOGEXT logging macro from: http://iphoneincubator.com/blog/debugging/the-evolution-of-a-replacement-for-nslog
+/*
+ The MIT License
+ 
+ Copyright (c) 2009 Free Time Studios and Nathan Eror
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+*/ 
+
+// FTLOGEXT logging macro cribbed from: http://iphoneincubator.com/blog/debugging/the-evolution-of-a-replacement-for-nslog
 #ifdef DEBUG
   #define FTLOG(...) NSLog(__VA_ARGS__)
   #define FTLOGEXT(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
--- main.m
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-
-int main(int argc, char *argv[]) {
-  NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-  int retVal = UIApplicationMain(argc, argv, nil, @"FTUtilsAppDelegate");
-  [pool release];
-  return retVal;
-}
